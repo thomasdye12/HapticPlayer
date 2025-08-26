ffaf60b7063edb2dc43bb9438423c17119387bca
Thomas <Thomas@thomasdye.net>
2025-08-26 02:43:07 +0100

    Initial Commit

---
+++ HapticPlayer/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HapticPlayer
+//
+//  Created by Thomas Dye on 26/08/2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
